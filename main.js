7c3232f20bea46df527c4789ef237c65e5cadabb
Amine Boukhari <aminetech21@outlook.com>
2021-12-17 14:31:45 +0100

    touch-screen-hackathon

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function initMap(){
 
     //Map options
     var options = {
-        zoom:10,
+        zoom:12,
         center:{lat:35.6987,lng:-0.6349}
     }
     
@@ -38,7 +38,7 @@ function initMap(){
         },
         {
             coords:{lat:35.70027440982651,lng:-0.6499302542152523},
-            IconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+            IconImage:'images/test_test_4.svg',
             content:`<h1>Mosquée</h1> <br><div class="about-section-pop-up"><div class="grid-item1-pop-up"><img src="images/mosque-ben-abdullah.png" alt=""></div><div class="grid-item2-pop-up"><h2>About</h2> <p>Je suis une section about</p></div></div>`
 
 
